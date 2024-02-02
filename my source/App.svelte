4032c4dce3b80b1b9c841bd4f720d1bc9694aa63
NeonD00m <neond00m.business@gmail.com>
2024-02-02 13:12:19 -0800

    Initial commit

---
+++ my source/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	let count = 0;
+	$: doubled = count * 2;
+
+	function increment() {
+		count += 1;
+	}
+
+	import PackageInfo from './PackageInfo.svelte';
+
+	const pkg = {
+		name: 'svelte',
+		speed: 'blazing',
+		version: 4,
+		website: 'https://svelte.dev'
+	};
+</script>
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
+<p>{count} doubled is {doubled}</p>
+
+<PackageInfo
+	name={pkg.name}
+	speed={pkg.speed}
+	website={pkg.website}
+/>
